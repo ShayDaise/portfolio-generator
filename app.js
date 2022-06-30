ad9bc62d9a6620c48341d24ae7c76633342e39ea
ShayDaise <102685776+ShayDaise@users.noreply.github.com>
2022-06-30 14:44:02 -0400

    finished project

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import inquirer from 'inquirer';
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+
+const { writeFile, copyFile } = require('./utils/generate-site.js');
+const generatePage = require('./src/page-template');
 
 
 
@@ -129,13 +130,20 @@ Add a New Project
 
 promptUser()
   .then(promptProject)
-  .then(portfolioData => 
-    console.log(portfolioData));
-
-  // const pageHTML = generatePage(portfolioData);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// }); 
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+  
